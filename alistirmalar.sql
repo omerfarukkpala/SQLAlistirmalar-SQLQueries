a4948b4edcdfa62576e865af23862efc7d3ce622
omerfarukkpala <omerfarukkpala@gmail.com>
2022-11-17 17:30:26 +0300

    genel bilgi

--- alistirmalar.sql
+++ alistirmalar.sql
@@ -1,8 +1,12 @@
 -- başla
 /*
---RDMBS :İlişkisel veritabanı yönetim sistemi
+DMBS :İlişkisel veritabanı yönetim sistemi
+T-SQL Dili:Transact Structurer Query Language
 
-
+master:veritabanlarının ve ayarlarının tutulduğu database'dir.
+model:Veritabanı şemalarının tutulduğu databasedir.
+msdb:Server ile ilgili ayarların tutulduğu database'dir.
+tempdb:Geçici database veya tabloların tutulduığu database'dir.
 
 
  */
