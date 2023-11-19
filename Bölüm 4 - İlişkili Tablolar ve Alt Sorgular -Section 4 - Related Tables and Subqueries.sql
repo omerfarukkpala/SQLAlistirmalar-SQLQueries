8d7339dfa5c03048d0481b255467fd0b3d976aff
= <omerfarukkpala@gmail.com>
2023-11-19 23:00:50 +0300

    update

--- Bölüm 4 - İlişkili Tablolar ve Alt Sorgular -Section 4 - Related Tables and Subqueries.sql
+++ Bölüm 4 - İlişkili Tablolar ve Alt Sorgular -Section 4 - Related Tables and Subqueries.sql
@@ -1,3 +1,6 @@
 /*Bölüm 4 - İlişkili Tablolar ve Alt Sorgular -
 Section 4 - Related Tables and Subqueries
 */
+
+--UPDATE TBLOGRENCILER SET OGRKULUP=1 WHERE OGRKULUP='Bilişim'
+--UPDATE TBLOGRENCILER SET OGRKULUP=2 WHERE OGRKULUP='Kitaplık'
