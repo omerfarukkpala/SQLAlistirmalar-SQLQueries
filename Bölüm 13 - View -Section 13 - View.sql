86678e56b08ff61ed7cef47237f709efc644bbdd
= <omerfarukkpala@gmail.com>
2023-11-22 12:05:31 +0300

    Section 13 - View

---
+++ Bölüm 13 - View -Section 13 - View.sql
@@ -0,0 +1,35 @@
+/*Bölüm 13 - View -
+Section 13 - View */
+--VIEW OLUŞTURMA VE KULLANIMI
+
+--CREATE VIEW TEST1
+--AS
+--SELECT * FROM TBLKATEGORI
+
+--SELECT * FROM TEST1
+--ALTER VIEW TEST1
+--AS
+--SELECT * FROM TBLKATEGORI WHERE KATEGORIID < 4
+--SELECT * FROM TEST1
+
+--SELECT * FROM TEST2
+
+--SELECT * FROM TEST3
+--TEST3 örneğinde şehri Adana olan müşterilerin hareket tablosundaki değerleri yazdıran sorgu
+
+
+--Section 13 - View 
+--CREATE VIEW TEST1
+--AS
+--SELECT * FROM CATEGORIES
+
+--SELECT * FROM TEST1
+--ALTER VIEW TEST1
+--AS
+--SELECT * FROM CATEGORIES WHERE CATEGORYID < 4
+--SELECT * FROM TEST1
+
+--SELECT * FROM TEST2
+
+--SELECT * FROM TEST3
+--Query to display the values from the transaction table for customers whose city is Adana in the TEST3 example.
