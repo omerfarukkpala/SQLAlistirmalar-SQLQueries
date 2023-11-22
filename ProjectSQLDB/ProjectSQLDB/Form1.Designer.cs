da8c2e945e07d5211cd54651ca9fd81cec49bd3b
= <omerfarukkpala@gmail.com>
2023-11-22 23:34:37 +0300

    form 1 added.

--- ProjectSQLDB/ProjectSQLDB/Form1.Designer.cs
+++ ProjectSQLDB/ProjectSQLDB/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(54)))), ((int)(((byte)(80)))));
+            this.Panel2.Location = new System.Drawing.Point(175, -16);
+            this.Panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(694, 51);
+            this.Panel2.TabIndex = 7;
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(54)))), ((int)(((byte)(80)))));
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel1.Location = new System.Drawing.Point(0, 0);
+            this.Panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(261, 824);
+            this.Panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(866, 824);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.Panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        internal System.Windows.Forms.Panel Panel2;
+        internal System.Windows.Forms.Panel Panel1;
     }
 }
 
