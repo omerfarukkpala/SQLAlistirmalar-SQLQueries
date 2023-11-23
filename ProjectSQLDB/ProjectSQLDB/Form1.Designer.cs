543fbadf204e25058f8ed0bfb91c6d196819ae48
= <omerfarukkpala@gmail.com>
2023-11-23 10:40:16 +0300

    .

--- ProjectSQLDB/ProjectSQLDB/Form1.Designer.cs
+++ ProjectSQLDB/ProjectSQLDB/Form1.Designer.cs
@@ -35,10 +35,9 @@
             // Panel2
             // 
             this.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(54)))), ((int)(((byte)(80)))));
-            this.Panel2.Location = new System.Drawing.Point(175, -16);
-            this.Panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.Panel2.Location = new System.Drawing.Point(262, -22);
             this.Panel2.Name = "Panel2";
-            this.Panel2.Size = new System.Drawing.Size(694, 51);
+            this.Panel2.Size = new System.Drawing.Size(1041, 71);
             this.Panel2.TabIndex = 7;
             // 
             // Panel1
@@ -46,18 +45,19 @@
             this.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(54)))), ((int)(((byte)(80)))));
             this.Panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.Panel1.Location = new System.Drawing.Point(0, 0);
-            this.Panel1.Margin = new System.Windows.Forms.Padding(2);
             this.Panel1.Name = "Panel1";
-            this.Panel1.Size = new System.Drawing.Size(261, 824);
+            this.Panel1.Size = new System.Drawing.Size(392, 1061);
             this.Panel1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(866, 824);
+            this.ClientSize = new System.Drawing.Size(1299, 1061);
             this.Controls.Add(this.Panel2);
             this.Controls.Add(this.Panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
