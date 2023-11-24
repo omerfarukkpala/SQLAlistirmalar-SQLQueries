97b711c9e758e06da7886673dde1613009ddf45b
= <omerfarukkpala@gmail.com>
2023-11-24 22:12:33 +0300

    .

--- ProjectSQLDB/ProjectSQLDB/FrmMusteri.Designer.cs
+++ ProjectSQLDB/ProjectSQLDB/FrmMusteri.Designer.cs
@@ -28,21 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMusteri));
+            this.PictureBox4 = new System.Windows.Forms.PictureBox();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.PictureBox3 = new System.Windows.Forms.PictureBox();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.TxtKategoriAd = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TxtKategoriId = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // PictureBox4
+            // 
+            this.PictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox4.Image")));
+            this.PictureBox4.Location = new System.Drawing.Point(641, 233);
+            this.PictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.PictureBox4.Name = "PictureBox4";
+            this.PictureBox4.Size = new System.Drawing.Size(75, 36);
+            this.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox4.TabIndex = 51;
+            this.PictureBox4.TabStop = false;
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuncelle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnGuncelle.Location = new System.Drawing.Point(721, 233);
+            this.BtnGuncelle.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(176, 36);
+            this.BtnGuncelle.TabIndex = 50;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox3.Image")));
+            this.PictureBox3.Location = new System.Drawing.Point(641, 193);
+            this.PictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(75, 36);
+            this.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox3.TabIndex = 49;
+            this.PictureBox3.TabStop = false;
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSil.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnSil.Location = new System.Drawing.Point(721, 193);
+            this.BtnSil.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(176, 36);
+            this.BtnSil.TabIndex = 48;
+            this.BtnSil.Text = "SİL";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox2.Image")));
+            this.PictureBox2.Location = new System.Drawing.Point(641, 152);
+            this.PictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(75, 36);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 47;
+            this.PictureBox2.TabStop = false;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnKaydet.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnKaydet.Location = new System.Drawing.Point(721, 152);
+            this.BtnKaydet.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(176, 36);
+            this.BtnKaydet.TabIndex = 46;
+            this.BtnKaydet.Text = "KAYDET";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(641, 112);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(75, 36);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 45;
+            this.PictureBox1.TabStop = false;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnListele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnListele.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnListele.Location = new System.Drawing.Point(721, 112);
+            this.BtnListele.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(176, 36);
+            this.BtnListele.TabIndex = 44;
+            this.BtnListele.Text = "LİSTELEME";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            // 
+            // TxtKategoriAd
+            // 
+            this.TxtKategoriAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKategoriAd.Location = new System.Drawing.Point(433, 193);
+            this.TxtKategoriAd.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtKategoriAd.Multiline = true;
+            this.TxtKategoriAd.Name = "TxtKategoriAd";
+            this.TxtKategoriAd.Size = new System.Drawing.Size(188, 35);
+            this.TxtKategoriAd.TabIndex = 43;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Label2.ForeColor = System.Drawing.Color.White;
+            this.Label2.Location = new System.Drawing.Point(313, 205);
+            this.Label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(123, 20);
+            this.Label2.TabIndex = 42;
+            this.Label2.Text = "KATEGORİ AD:";
+            // 
+            // TxtKategoriId
+            // 
+            this.TxtKategoriId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKategoriId.Location = new System.Drawing.Point(433, 153);
+            this.TxtKategoriId.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtKategoriId.Multiline = true;
+            this.TxtKategoriId.Name = "TxtKategoriId";
+            this.TxtKategoriId.Size = new System.Drawing.Size(188, 35);
+            this.TxtKategoriId.TabIndex = 41;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Label1.ForeColor = System.Drawing.Color.White;
+            this.Label1.Location = new System.Drawing.Point(320, 165);
+            this.Label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(117, 20);
+            this.Label1.TabIndex = 40;
+            this.Label1.Text = "KATEGORİ ID:";
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(292, 320);
+            this.DataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersWidth = 51;
+            this.DataGridView1.RowTemplate.Height = 24;
+            this.DataGridView1.Size = new System.Drawing.Size(616, 261);
+            this.DataGridView1.TabIndex = 39;
             // 
             // FrmMusteri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.PictureBox4);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.PictureBox3);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.TxtKategoriAd);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.TxtKategoriId);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.DataGridView1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMusteri";
             this.Text = "FrmMusteri";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        internal System.Windows.Forms.PictureBox PictureBox4;
+        internal System.Windows.Forms.Button BtnGuncelle;
+        internal System.Windows.Forms.PictureBox PictureBox3;
+        internal System.Windows.Forms.Button BtnSil;
+        internal System.Windows.Forms.PictureBox PictureBox2;
+        internal System.Windows.Forms.Button BtnKaydet;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button BtnListele;
+        internal System.Windows.Forms.TextBox TxtKategoriAd;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TxtKategoriId;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.DataGridView DataGridView1;
     }
 }
