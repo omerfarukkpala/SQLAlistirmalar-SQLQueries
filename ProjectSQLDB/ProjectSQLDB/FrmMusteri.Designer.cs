154fe77d1cc9df348d8d6dfec83325e864d0ec52
= <omerfarukkpala@gmail.com>
2023-11-24 22:11:23 +0300

    .

--- ProjectSQLDB/ProjectSQLDB/FrmMusteri.Designer.cs
+++ ProjectSQLDB/ProjectSQLDB/FrmMusteri.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMusteri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMusteri";
             this.Text = "FrmMusteri";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
